b64f8ea30d460d14a5f69a09d57b7c7bbeece13f
Michal Dyzma <mdyzma@gmail.com>
2021-07-24 22:13:24 +0200

    Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Michal Dyzma"
 author = "Michal Dyzma"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.1"
 
 
 # -- General configuration ---------------------------------------------------
